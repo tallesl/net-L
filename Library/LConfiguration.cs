ddb001c0c6d18c7251e500b232f2a8ac989a8b65
Talles L <talleslasmar@gmail.com>
2017-06-11 22:55:24 -0300

    who's gonna compare a configuration object anyway?

--- Library/LConfiguration.cs
+++ Library/LConfiguration.cs
@@ -6,6 +6,7 @@
     /// <summary>
     /// Optional configurations that can be used when initializing the logger.
     /// </summary>
+    [SuppressMessage("Microsoft.Performance", "CA1815:OverrideEqualsAndOperatorEqualsOnValueTypes")]
     public struct LConfiguration
     {
         /// <summary>
@@ -37,29 +38,7 @@
         /// An attempt to log with a label that is not enabled is simply ignored, no error is raised.
         /// Leave it empty or null to enable any label, which is the default.
         /// </summary>
-        [SuppressMessage("Microsoft.Performance", "CA1819:PropertiesShouldNotReturnArrays",
-            Justification = "The object it's passed by value (since it's a struct) plus is used just as configuration.")]
+        [SuppressMessage("Microsoft.Performance", "CA1819:PropertiesShouldNotReturnArrays")]
         public string[] EnabledLabels { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            return obj is LConfiguration ? this == (LConfiguration)obj : false;
-        }
-
-        public override int GetHashCode()
-        {
-            return UseUtcTime.GetHashCode();
-        }
-
-        public static bool operator ==(LConfiguration a, LConfiguration b)
-        {
-            return a.UseUtcTime == b.UseUtcTime &&
-                a.DeleteOldFiles == b.DeleteOldFiles;
-        }
-
-        public static bool operator !=(LConfiguration a, LConfiguration b)
-        {
-            return !(a == b);
-        }
     }
 }
